56c2b8082c12deb4e8b0b2c46ce575194e8cf221
Lucas Silva Souza <lucassilvadonato23@gmail.com>
2021-12-16 20:51:24 -0300

    salve

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const ejs = require('ejs')
 const app = express()
 
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    res.render('index', { nome: "Lucas"})
-})
+app.get('/', (req, res) => res.render('index'))
+
+app.get('/perguntar', (req, res) => res.render('perguntar'))
 
 app.listen(8080, (req, res) => console.log('listening on port 8080'))
